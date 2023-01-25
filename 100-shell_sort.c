4858dd686e97330d4bd5a3e83d73bb8f8a9c5044
Your Name <you@example.com>
2023-01-25 02:48:12 -0800

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,6 +11,9 @@ void shell_sort(int *array, size_t size)
 	size_t interval, i, j;
 	int temp;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (interval = size / 2; interval > 0; interval /= 2)
 	{
 		for (i = interval; i < size; i += 1)
